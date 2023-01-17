f69a2b2837e8295a0ca89fdb2ff98c9485742c26
jeone99ran <122783837+jeone99ran@users.noreply.github.com>
2023-01-17 17:55:46 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+document.addEventListener('scroll', () => {
+  console.log(window.scrollY);
+  console.log(`navbarHeight : ${navbarHeight}`);
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add('navbar--dark');
+  } else {
+    navbar.classList.remove('navbar--dark');
+  }
+});
+
+// Handle scrolling
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  console.log(event.target.dataset.link);
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
+});
